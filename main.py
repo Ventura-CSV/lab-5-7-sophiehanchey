4fcab9ab570caf36bd911dd9535599cb8c2458fd
Sophie Hanchey <sophiehanchey@gmail.com>
2024-07-02 14:13:58 -0700

    initial code, not able to unpack

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 
 def splitlist(numbers):
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+    # split the numbers up using * and check the output
+    splitNums = [*numbers]
+    print(f'The numbers: {splitNums}')
+    
+    # find the minimum and idx of minimum
+    min = None
+    minIdx = None
+    currentIdx = 0
+    for n in splitNums:
+        if min is None or n < min:
+            min = n
+            minIdx = currentIdx
+        currentIdx += 1
+        
+    # swap the minimum value with the first item in list
+    t = splitNums[minIdx]
+    del splitNums[minIdx]
+    print(f'{t}, {splitNums}')
 
 
 def main():
